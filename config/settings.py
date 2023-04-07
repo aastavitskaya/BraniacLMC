d0b1d086033bd88a265712d9061d73db1140532e
Anastasia <aastavitskaya@gmail.com>
2023-04-07 11:26:08 +0400

    lesson_6 email authenticate

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,8 @@ AUTH_USER_MODEL = "authapp.CustomUser"
 AUTHENTICATION_BACKENDS = (
     "social_core.backends.github.GithubOAuth2",
     "social_core.backends.vk.VKOAuth2",
-    "django.contrib.auth.backends.ModelBackend",
+    "authapp.auth.EmailAuthBackend",
+    # "django.contrib.auth.backends.ModelBackend",
 )
 
 LOGIN_REDIRECT_URL = "mainapp:main_page"
