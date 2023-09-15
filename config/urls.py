868ccbe5b9ad7448af087488f435cc9a8de6fc59
Anastasia <aastavitskaya@gmail.com>
2023-09-15 22:44:43 +0400

    lesson_8 i18n

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("", RedirectView.as_view(url="mainapp/")),
     path("social_auth/", include("social_django.urls", namespace="social")),
     path("mainapp/", include("mainapp.urls", namespace="mainapp")),
